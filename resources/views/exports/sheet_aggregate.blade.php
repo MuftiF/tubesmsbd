bdbad22cd569031e9439eb3be36f2fb7a12c8f60
RIONAIBAHO <rionaibaho173@gmail.com>
2025-12-13 07:32:14 +0700

    update baru lagi

--- resources/views/exports/sheet_aggregate.blade.php
+++ resources/views/exports/sheet_aggregate.blade.php
@@ -27,54 +27,56 @@
 </style>
 
 <table>
-   <thead>
+    <thead>
+        {{-- ====== KELOMPOK TANGGAL PER BULAN ====== --}}
+        @php
+            $months = [];
+            foreach ($dates as $d) {
+                $carbon = \Carbon\Carbon::parse($d);
+                $monthKey = $carbon->format('Y-m');
+                $months[$monthKey][] = $d;
+            }
+            
+            // Filter user: hanya yang bukan admin atau manager
+            $filtered_users = $users->filter(function($user) {
+                return !in_array($user->role, ['admin', 'manager']);
+            });
+        @endphp
 
-    {{-- ====== KELOMPOK TANGGAL PER BULAN ====== --}}
-    @php
-        $months = [];
-        foreach ($dates as $d) {
-            $carbon = \Carbon\Carbon::parse($d);
-            $monthKey = $carbon->format('Y-m');
-            $months[$monthKey][] = $d;
-        }
-    @endphp
+        {{-- ====== HEADER BARIS 1: Nama Pegawai + Bulan ====== --}}
+        <tr>
+            <th rowspan="2">Nama Pegawai</th>
+            <th rowspan="2">Role</th>
+            <th rowspan="2">No HP</th>
 
-    {{-- ====== HEADER BARIS 1: Nama Pegawai + Bulan ====== --}}
-    <tr>
-        <th rowspan="2">Nama Pegawai</th>
-        <th rowspan="2">Role</th>
-        <th rowspan="2">No HP</th>
+            @foreach($months as $monthKey => $ds)
+                @php
+                    $m = \Carbon\Carbon::parse($ds[0])->translatedFormat('F Y');
+                    $countDays = count($ds);
+                @endphp
 
-        @foreach($months as $monthKey => $ds)
-            @php
-                $m = \Carbon\Carbon::parse($ds[0])->translatedFormat('F Y');
-                $countDays = count($ds);
-            @endphp
-
-            <th colspan="{{ $countDays }}">{{ $m }}</th>
-        @endforeach
-
-        <th rowspan="2">Total Tandan</th>
-        <th rowspan="2">Total Berat (kg)</th>
-    </tr>
-
-    {{-- ====== HEADER BARIS 2: Nomor Tanggal ====== --}}
-    <tr>
-        @foreach($months as $monthKey => $ds)
-            @foreach($ds as $d)
-                <th>{{ \Carbon\Carbon::parse($d)->format('d') }}</th>
+                <th colspan="{{ $countDays }}">{{ $m }}</th>
             @endforeach
-        @endforeach
-    </tr>
 
-</thead>
+            {{-- Hanya Total Berat saja --}}
+            <th rowspan="2">Total Berat (kg)</th>
+        </tr>
 
+        {{-- ====== HEADER BARIS 2: Nomor Tanggal ====== --}}
+        <tr>
+            @foreach($months as $monthKey => $ds)
+                @foreach($ds as $d)
+                    <th>{{ \Carbon\Carbon::parse($d)->format('d') }}</th>
+                @endforeach
+            @endforeach
+        </tr>
+    </thead>
 
     <tbody>
-        @foreach($users as $u)
+        {{-- Tampilkan hanya user yang bukan admin/manager --}}
+        @foreach($filtered_users as $u)
             @php
-                $total_tandan = 0;
-                $total_berat  = 0;
+                $total_berat  = 0; // Hanya inisialisasi total berat
             @endphp
 
             <tr>
@@ -83,7 +85,6 @@
                 <td>{{ $u->no_hp }}</td>
 
                 @foreach($dates as $d)
-
                     @php
                         $mark = '';
                         $key  = $u->id . '-' . $d;
@@ -91,8 +92,7 @@
                         if(isset($panen[$key])) {
                             $row = $panen[$key][0];
                             $mark = 'H';
-                            $total_tandan += (int) $row->jumlah_tandan;
-                            $total_berat  += (float) $row->berat_kg;
+                            $total_berat  += (float) $row->berat_kg; // Hanya tambah berat
                         }
 
                         if(!$mark && isset($absensi[$key])) {
@@ -100,12 +100,34 @@
                         }
                     @endphp
 
-                    <td>{{ $mark }}</td>
+                    {{-- ====== WARNA SESUAI STATUS ====== --}}
+                    <td 
+                        @php
+                            if ($mark === 'H') {
+                                echo 'style="background:#c6efce; color:#006100;"'; // Hijau
+                            } elseif ($mark === 'T') {
+                                echo 'style="background:#ffeb9c; color:#9c6500;"'; // Kuning
+                            } else {
+                                echo 'style="background:#f2b3b3; color:#7f0000;"'; // Merah (Alpha)
+                            }
+                        @endphp
+                    >
+                        {{ $mark ?: 'A' }}
+                    </td>
                 @endforeach
 
-                <td>{{ $total_tandan }}</td>
+                {{-- Hanya tampilkan total berat --}}
                 <td>{{ number_format($total_berat, 2) }}</td>
             </tr>
         @endforeach
+        
+        {{-- Pesan jika tidak ada data --}}
+        @if($filtered_users->count() === 0)
+            <tr>
+                <td colspan="{{ count($dates) + 4 }}" style="text-align: center; padding: 20px;">
+                    Tidak ada data pegawai yang ditampilkan (admin dan manager tidak termasuk)
+                </td>
+            </tr>
+        @endif
     </tbody>
 </table>
